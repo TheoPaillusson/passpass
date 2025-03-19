48bc5c3258759291dea05597091710d432d35bbf
Theo Paillusson <theo.paillusson@gmail.com>
2025-03-19 18:38:33 +0100

    setup database structure + models creation

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import create_app, db
+from flask import render_template
+from app.models import User, Subject, Chapter, Quiz, Question, Score
+
+app = create_app()
+
+@app.cli.command('db-create')
+def create_db():
+    db.create_all()
+    print("Database created")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
